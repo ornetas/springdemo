9e3842f8a671c82524bdbf31d775c08e3c110207
orentas <o.b.ornetas@gmail.com>
2023-01-16 10:37:13 +0200

    Initial commit

---
+++ src/test/java/lt/ornetas/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.ornetas.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
