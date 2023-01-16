28143f9ebd79904a6521346b821c6f44be227ff4
orentas <o.b.ornetas@gmail.com>
2023-01-16 15:57:10 +0200

    Get all customers from mapped controller

--- src/main/java/lt/ornetas/springdemo/controller/CustomerController.java
+++ src/main/java/lt/ornetas/springdemo/controller/CustomerController.java
@@ -1,17 +1,33 @@
 package lt.ornetas.springdemo.controller;
 
+import lt.ornetas.springdemo.repository.model.Customer;
+import lt.ornetas.springdemo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller // https://localhost:8080/customermapping/test
-@RequestMapping(path = "/customermapping")
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/customermapping") //localhost:8080/customermapping
 public class CustomerController{
 
+    @Autowired
+    private CustomerService customerService;
+
+    // https://localhost:8080/customermapping/test
     @GetMapping( path = "/test" )
     public @ResponseBody String getTestPage(){
         return "This is some test from SPRING!";
+    }
+    // http://localhost:8080/customermapping/customer/all
+    @GetMapping( path = "/customer/all" )
+    public @ResponseBody List<Customer> getAllCustomers(){
+        return customerService.getAllCustomers();
+
+
     }
 
 
