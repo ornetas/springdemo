e896327f4f64341be063c2080135ca086a447bcb
orentas <o.b.ornetas@gmail.com>
2023-01-16 14:44:12 +0200

    Implement lombok annotations

--- src/main/java/lt/ornetas/springdemo/repository/model/ProductLine.java
+++ src/main/java/lt/ornetas/springdemo/repository/model/ProductLine.java
@@ -4,9 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table (name = "productlines")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class ProductLine {
 
     @Id
@@ -17,36 +25,13 @@ public class ProductLine {
     @Column (name = "htmldesription")
     private String htmlDescription;
 
-    public String getProductLine() {
-        return productLine;
-    }
 
-    public void setProductLine(String productLine) {
-        this.productLine = productLine;
-    }
-
-    public String getTextDescription() {
-        return textDescription;
-    }
-
-    public void setTextDescription(String textDescription) {
-        this.textDescription = textDescription;
-    }
-
-    public String getHtmlDescription() {
-        return htmlDescription;
-    }
-
-    public void setHtmlDescription(String htmlDescription) {
-        this.htmlDescription = htmlDescription;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                ", Product line: " + productLine + '\'' +
-                ", description:'" + textDescription + '\'' +
-                ", html description: '" + htmlDescription + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "{" +
+//                ", Product line: " + productLine + '\'' +
+//                ", description:'" + textDescription + '\'' +
+//                ", html description: '" + htmlDescription + '\'' +
+//                '}';
+//    }
 }
