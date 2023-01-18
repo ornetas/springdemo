88b2af71b1932b0045205e4e49551f738c387efe
orentas <o.b.ornetas@gmail.com>
2023-01-18 09:03:31 +0200

    Added costumer info

--- src/main/java/lt/ornetas/springdemo/controller/CustomerTemplateController.java
+++ src/main/java/lt/ornetas/springdemo/controller/CustomerTemplateController.java
@@ -1,9 +1,12 @@
 package lt.ornetas.springdemo.controller;
 
+import lt.ornetas.springdemo.repository.model.Customer;
 import lt.ornetas.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,4 +22,19 @@ public class CustomerTemplateController {
 
         return "/test/test_page";
     }
+
+    // http://localhost:8080/customertemplate/firstpage/customer/112
+    @GetMapping(path = "/firstpage/customer/{id}")
+    public String getCustomerInFirstPage(Model model, @PathVariable int id) {
+
+       Customer customer = customerService.getMyCustomerById(id);
+       model.addAttribute("company_name", customer.getCustomerName());
+       model.addAttribute("contact_last_name", customer.getContactLastName());
+       model.addAttribute("contact_first_name", customer.getContactFirstName());
+        model.addAttribute("country", customer.getCountry());
+
+        return "/test/firstpage";
+    }
+
+
 }
